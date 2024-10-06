530c070ced597b822c89e0c291bbbe9d62f8bee7
AMD <andreyarroyo02@gmail.com>
2024-10-05 20:49:01 -0600

    Idiomas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { useTranslation } from "react-i18next";
 import "./App.css";
 
 function App() {
-  return <>Hello world</>;
+  const { t } = useTranslation();
+
+  return <h2>{t("welcome_to_react")}</h2>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ["en", "es"],
+    fallbackLng: "es",
+    detection: {
+      order: ["htmlTag", "localStorage", "querystring", "cookie", "navigator"],
+      caches: ["cookie"],
+    },
+    backend: { loadPath: "/assets/locals/{{lng}}/translation.json" },
+  });
+
+// append app to dom
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
