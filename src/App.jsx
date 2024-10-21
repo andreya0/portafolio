2b98f256b2fa53836e23650fc65d4cd5e2e63ebd
AMD <andreyarroyo02@gmail.com>
2024-10-21 17:20:33 -0600

    404 responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,26 @@ import ContactPage from "./pages/ContactPage";
 import AboutPage from "./pages/AboutPage";
 import WorkPage from "./pages/WorkPage";
 import Navbar from "./components/Navbar";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   const location = useLocation();
   const isHomePage = location.pathname == "/";
+  const isFound =
+    location.pathname === "/" ||
+    location.pathname === "/about" ||
+    location.pathname === "/work" ||
+    location.pathname === "/contact";
   return (
     <>
-      {isHomePage ? <LanguageSwitcher /> : <Navbar />}
+      {isFound && (isHomePage ? <LanguageSwitcher /> : <Navbar />)}
 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/work" element={<WorkPage />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
